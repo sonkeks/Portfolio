26a4a03bfda28874cdb4891f21581b6721c7a22e
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2023-08-14 16:20:38 +0200

    form validation & user feedback

--- src/newComponents/Contact.tsx
+++ src/newComponents/Contact.tsx
@@ -1,6 +1,7 @@
 import styles from "./Contact.module.css"
-import {SyntheticEvent, useState} from "react";
+import {ChangeEvent, SyntheticEvent, useState} from "react";
 import emailjs from "@emailjs/browser";
+import {FiCheckCircle, FiXCircle} from "react-icons/fi";
 
 const Contact = () => {
     const [name, setName] = useState<string>("");
@@ -8,6 +9,31 @@ const Contact = () => {
     const [email, setEmail] = useState<string>("");
     const [subject, setSubject] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+
+    const [nameError, setNameError] = useState<string>("");
+    const [surnameError, setSurnameError] = useState<string>("");
+    const [emailError, setEmailError] = useState<string>("");
+    const [subjectError, setSubjectError] = useState<string>("");
+    const [messageError, setMessageError] = useState<string>("");
+
+    const [isSending, setIsSending] = useState<boolean>(false);
+    const [emailSent, setEmailSent] = useState<boolean>();
+
+    const formIsValid = () => {
+        let fields = [name, surname, email, subject, message];
+        let errors = [nameError, surnameError, emailError, subjectError, messageError];
+        return (errors.every(element => element === "") && fields.every(field => field !== ""));
+    }
+
+    const changeField = (e: ChangeEvent<(HTMLInputElement | HTMLTextAreaElement)>, stateFunction: (state: string) => void, errorFunction: (error: string) => void) => {
+        let fieldName = e.target.name;
+        stateFunction(e.target.value);
+        if (e.target.value === "") {
+            errorFunction("Please add your " + fieldName.charAt(0).toUpperCase() + fieldName.slice(1));
+        } else {
+            errorFunction("");
+        }
+    }
 
     const submitForm = (event: SyntheticEvent) => {
         event.preventDefault();
@@ -17,13 +43,44 @@ const Contact = () => {
             from_email: email,
             message: message,
         }
+        setIsSending(true);
         emailjs.send("service_48dpmw2", "template_i2ywh58", params, "UqwU8RRV6d3zX77CS")
-            .then((result) => {
-                console.log(result.text);
+            .then(() => {
+                setName("");
+                setSurname("");
+                setEmail("");
+                setSubject("");
+                setMessage("");
+                setIsSending(false);
+                setEmailSent(true);
+                setTimeout(() => setEmailSent(undefined), 10000);
             })
             .catch(error => {
+                setIsSending(false);
+                setEmailSent(false);
+                setTimeout(() => setEmailSent(undefined), 10000);
                 console.log(error.text);
             })
+    }
+
+    const showConfirmation = () => {
+        if (emailSent !== undefined) {
+            if (emailSent) {
+                return (
+                    <>
+                        <FiCheckCircle className={styles.successIcon} size={30}/>
+                        <span>Email was successfully sent</span>
+                    </>
+                )
+            } else {
+                return (
+                    <>
+                        <FiXCircle className={styles.errorIcon} size={30}/>
+                        <span>Sorry, something went wrong. Please try again</span>
+                    </>
+                )
+            }
+        }
     }
 
     const clearForm = (event: SyntheticEvent) => {
@@ -33,6 +90,12 @@ const Contact = () => {
         setEmail("");
         setSubject("");
         setMessage("");
+
+        setNameError("");
+        setSurnameError("");
+        setEmailError("");
+        setSubjectError("");
+        setMessageError("");
     }
 
     return (
@@ -41,32 +104,48 @@ const Contact = () => {
             <h2>Let's get in Touch</h2>
             <form onSubmit={submitForm} className={styles.form}>
                 <div className={styles.inputGroup}>
-                    <input autoComplete="off" placeholder="Name" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input autoComplete="off" placeholder="Name" id="name" name="name" value={name}
+                           onChange={(e) => changeField(e, setName, setNameError)}/>
                     <label htmlFor="name" className={styles.label}>Name</label>
+                    <small className={styles.errorMessage}>{nameError}</small>
                 </div>
                 <div className={styles.inputGroup}>
-                    <input autoComplete="off" placeholder="Surname" id="surname" name="surname" value={surname} onChange={(e) => setSurname(e.target.value)}/>
+                    <input autoComplete="off" placeholder="Surname" id="surname" name="surname" value={surname}
+                           onChange={(e) => changeField(e, setSurname, setSurnameError)}/>
                     <label htmlFor="surname" className={styles.label}>Surname</label>
+                    <small className={styles.errorMessage}>{surnameError}</small>
                 </div>
                 <div className={styles.inputGroup}>
-                    <input autoComplete="off" placeholder="Email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} type="email"/>
-                    <label htmlFor="email" className={styles.label}>Email address</label>
+                    <input autoComplete="off" placeholder="Email" id="email" name="email" value={email}
+                           onChange={(e) => changeField(e, setEmail, setEmailError)} type="email"/>
+                    <label htmlFor="email" className={styles.label}>Email Address</label>
+                    <small className={styles.errorMessage}>{emailError}</small>
                 </div>
                 <div className={styles.inputGroup}>
-                    <input autoComplete="off" placeholder="Subject" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)}/>
+                    <input autoComplete="off" placeholder="Subject" id="subject" name="subject" value={subject}
+                           onChange={(e) => changeField(e, setSubject, setSubjectError)}/>
                     <label htmlFor="subject" className={styles.label}>Subject</label>
+                    <small className={styles.errorMessage}>{subjectError}</small>
                 </div>
                 <div className={styles.inputGroup}>
-                    <textarea placeholder="Message" id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}/>
+                    <textarea placeholder="Message" id="message" name="message" value={message}
+                              onChange={(e) => changeField(e, setMessage, setMessageError)}/>
                     <label htmlFor="message" className={styles.textareaLabel}>Message</label>
+                    <small className={styles.errorMessage}>{messageError}</small>
                 </div>
                 <div className={styles.buttonRow}>
-                    <button onClick={event => clearForm(event)} className={styles.buttonClear}>
-                        Clear
-                    </button>
-                    <button type="submit" className={styles.button}>
-                        Send
-                    </button>
+                    <div className={styles.buttonGroup}>
+                        <button onClick={event => clearForm(event)} className={styles.buttonClear}>
+                            Clear
+                        </button>
+                        <button disabled={!formIsValid() || isSending} type="submit" className={styles.button}>
+                            {isSending && <span className={styles.loader}></span>}
+                            {!isSending && "Send"}
+                        </button>
+                    </div>
+                    <div className={styles.confirmationGroup}>
+                        {showConfirmation()}
+                    </div>
                 </div>
             </form>
         </div>
