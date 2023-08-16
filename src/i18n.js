e0f54c6397ea7d2c16a0b6e75a050b0a867f010e
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2023-08-16 17:53:52 +0200

    i18n env path switch

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,8 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
 
+console.log(process.env.NODE_ENV);
+
 i18n
     .use(initReactI18next)
     .use(Backend)
@@ -12,7 +14,7 @@ i18n
         debug: false,
         ns: ['contact', 'courses', 'experience', 'header', 'imprint', 'navbar', 'projects', 'projects', 'skills'],
         backend: {
-            loadPath: `/soenke/build/locales/{{lng}}/{{ns}}.json`
+            loadPath: process.env.NODE_ENV !== "production" ? `./locales/{{lng}}/{{ns}}.json` : `/soenke/build/locales/{{lng}}/{{ns}}.json`
         },
         load: 'unspecific',
         react: {
