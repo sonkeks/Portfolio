f4174091ed6970a8dfb8aac087ad48f8051b91e6
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2023-08-16 19:51:06 +0200

    add router for pretty url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route, Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  return (
-      <Home/>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/*' element={<Navigate replace to='/' />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
