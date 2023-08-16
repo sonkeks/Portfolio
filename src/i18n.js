506866b44876a998605c33e00e103ea83749e48c
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2023-08-16 17:55:25 +0200

    remove console.log

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,6 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
 
-console.log(process.env.NODE_ENV);
-
 i18n
     .use(initReactI18next)
     .use(Backend)
