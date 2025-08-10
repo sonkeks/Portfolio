851b91c861093aae2b211a17beb7cfd141bfcdf6
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2025-08-10 21:51:57 +0200

    Update Skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -9,63 +9,142 @@ import {
     DiPython,
     DiReact
 } from "react-icons/di";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import AOS from "aos";
 import 'aos/dist/aos.css';
 import {useTranslation} from "react-i18next";
+
+const LANGUAGES = [
+    "Java",
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "Swift",
+    "SQL",
+    "CLI",
+    "YAML"
+];
+
+const FRAMEWORKS = [
+    "React",
+    "Ionic",
+    "Next.js",
+    "Express.js",
+    "FastAPI",
+    "Spring",
+    "React Native",
+    "MongoDB",
+    "PostgreSQL",
+    "Firebase",
+    "Expo",
+    "Angular",
+    "React Testing Library"
+];
+
+const TOOLS = [
+    "Git",
+    "Google Cloud",
+    "Node.js",
+    "Maven",
+    "Capacitor",
+    "Foursquare",
+    "SpaCy",
+    "Docker",
+    "Figma",
+    "Maya"
+];
+
+const CONCEPTS = [
+    "JWT & Refresh Token",
+    "REST",
+    "State Management",
+    "ORM"
+];
 
 const Skills = () => {
     useEffect(() => {
         AOS.init();
     }, []);
-
+    const [highlightedCategory, setHighlightedCategory] = useState<number | undefined>();
+    const [activeCategory, setActiveCategory] = useState<number | undefined>(undefined);
+    
     const { t } = useTranslation("skills");
-
+    
+    const getShuffledSkills = () => {
+        const skills = [...LANGUAGES, ...FRAMEWORKS, ...TOOLS, ...CONCEPTS];
+        for (var i = skills.length - 1; i >= 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = skills[i];
+            skills[i] = skills[j];
+            skills[j] = temp;
+        }
+        return skills;
+    }
+    
+    const [skillsArray] = useState<string[]>(() => getShuffledSkills());
+    
+    const filterButton = (id: number, text: string) => {
+        const isActive = activeCategory === id;
+        const isHighlighted = highlightedCategory === id || highlightedCategory === undefined;
+        
+        return (
+          <button
+            className={isActive && isHighlighted ? styles.buttonFocussed : styles.buttonDefault}
+            onClick={() => {
+                setHighlightedCategory(() => !isActive ? id : undefined);
+                setActiveCategory(prev => prev !== id ? id : undefined);
+            }}
+            onMouseEnter={() => setHighlightedCategory(id)}
+            onMouseLeave={() => {
+                if (!isActive) {
+                    setHighlightedCategory(undefined);
+                }
+            }}
+          >
+              {text}
+          </button>
+        );
+    };
+    
+    const itemIsHighlighted = (item: string) => {
+        const index = highlightedCategory !== undefined ? highlightedCategory : activeCategory;
+        switch (index) {
+            case 0:
+                return LANGUAGES.includes(item);
+            case 1:
+                return FRAMEWORKS.includes(item);
+            case 2:
+                return TOOLS.includes(item);
+            case 3:
+                return CONCEPTS.includes(item);
+            default:
+                return false;
+        }
+    }
+    
     return (
-        <div className={styles.container} id="skills">
-            <div data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-out-cubic">
+        <div data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-out-cubic" className={styles.container} id="skills">
+            <div>
                 <h1>{t('skills')}</h1>
                 <h2>{t('subtitle')}</h2>
             </div>
             <div className={styles.box}>
-                <div data-aos="fade-right" data-aos-delay="0" data-aos-duration="1000" data-aos-easing="ease-out-sine">
-                    <h3>{t('languages')}</h3>
-                    <ul>
-                        <li>Java</li>
-                        <li>JavaScript</li>
-                        <li>TypeScript</li>
-                        <li>Python</li>
-                        <li>Swift</li>
-                        <li>SQL</li>
-                    </ul>
-                </div>
-                <div data-aos="fade-right" data-aos-delay="150" data-aos-duration="1000" data-aos-easing="ease-out-sine">
-                    <h3>Frameworks</h3>
-                    <ul>
-                        <li>React</li>
-                        <li>Ionic</li>
-                        <li>Next.js</li>
-                        <li>Express.js</li>
-                        <li>FastAPI</li>
-                        <li>Spring</li>
-                        <li>MongoDB</li>
-                        <li>Firebase</li>
-                    </ul>
-                </div>
-                <div data-aos="fade-right" data-aos-delay="300" data-aos-duration="1000" data-aos-easing="ease-out-sine">
-                    <h3>{t('tools')}</h3>
-                    <ul>
-                        <li>Git</li>
-                        <li>Node.js</li>
-                        <li>Capacitor</li>
-                        <li>SpaCy</li>
-                        <li>Docker</li>
-                        <li>Figma</li>
-                        <li>Maya</li>
+                <div>
+                    <div className={styles.buttons}>
+                        {filterButton(0, t('languages'))}
+                        {filterButton(1, t('frameworks'))}
+                        {filterButton(2, t('tools'))}
+                        {filterButton(3, t('concepts'))}
+                    </div>
+                    <ul className={styles.skillList}>
+                    {skillsArray.map((item, index) => (
+                          <li className={itemIsHighlighted(item) ? styles.highlighted : ""} key={index}>{item}</li>
+                        ))}
                     </ul>
                 </div>
             </div>
-            <div className={styles.backgroundBox} data-aos="zoom-in" data-aos-delay="0" data-aos-duration="2000" data-aos-easing="ease-out-sine">
+            <div className={styles.backgroundBox} data-aos="zoom-in" data-aos-delay="0" data-aos-duration="2000"
+                 data-aos-easing="ease-out-sine">
                 <DiReact/>
                 <DiJava/>
                 <DiJavascript1/>
